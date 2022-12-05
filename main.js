138a941c493d80211306010c11f10fb98c636e9a
kennethEdwin147 <edwingbeti@gmail.com>
2022-12-04 21:46:18 -0500

    'update'

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 (function () {
-
     const fs = require('fs');
 
     /* Get svg content */
     let animation = fs.readFileSync('svg/animate.svg').toString();
+    /* https://dopiaza.org/tools/datauri/index.php */
+    let font = fs.readFileSync('svg/font.svg').toString();
     let template = fs.readFileSync('svg/template.svg').toString();
 
-    /* Add animation et fonts */
+    /* Add animation and fonts */
     template = template.replace('{{animation}}', animation);
+    template = template.replace('{{font}}', font);
+    /* Add current Time */
+    template = template.replaceAll('{{date}}', getCurrentTime());
 
     /* Create a new svg */
     fs.writeFile("final.svg", template, (err) => {
         if (err)
-        console.log(err);
+            console.log(err);
         else {
-        console.log(fs.readFileSync("final.svg", "utf8"));
-        console.clear();
+            console.log(fs.readFileSync("final.svg", "utf8"));
+            console.clear();
         }
     });
 
+    function getCurrentTime() {
+        const options = {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+            timeZone: 'America/Toronto'
+        };
+        return new Intl.DateTimeFormat('fr-fr', options).format(new Date());
+    }
+
+    
 })();
